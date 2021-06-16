82a75d8a381dc164eb34724c38a220e0dcc3960a
rahulattainu <rahul@attainu.com>
2021-06-16 08:30:22 +0530

    added singleton pattern

---
+++ singleton/index.js
@@ -0,0 +1,29 @@
+function Process (state) {
+  	this.state = state;
+}
+
+const Singleton = (function () {
+    function ProcessManager () {
+        this.numProcess = 0
+    }
+
+    let pManager
+
+    function createProcessManager (){
+        pManager = new ProcessManager()
+        return pManager
+    }
+
+    return {
+        getProcessManager: () => {
+            if(!pManager)
+                pManager = createProcessManager()
+            return pManager
+
+        }
+    }
+})()
+
+const pm1 = Singleton.getProcessManager()
+const pm2 = Singleton.getProcessManager()
+console.log(pm1 === pm2)
